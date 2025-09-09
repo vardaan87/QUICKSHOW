3f36f01f729459c236a345c2f09ce7104597c21f
vardaan87 <mukulkumar5515@gmail.com>
2025-09-09 10:25:06 +0530

    fixed inngest function

--- sever/inngest/index.js
+++ sever/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/user";
+import User from "../models/user.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
